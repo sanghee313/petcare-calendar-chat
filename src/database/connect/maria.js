3c151b85da593a90bd9e14b613798f19ba23b060
LeeHuJong <ca0089@naver.com>
2023-06-07 18:43:51 +0900

    edit css

--- src/database/connect/maria.js
+++ src/database/connect/maria.js
@@ -4,7 +4,7 @@ const conn = maria.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "cho0313*",
+  password: "gnwhddldi2!",
   database: "petcare",
 });
 console.log("maria.db 연동");
